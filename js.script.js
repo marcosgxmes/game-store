92fc1a0ac29b1296da135fd6d1fd738378acb03e
marcosgxmes <marcosgms777@gmail.com>
2023-05-21 18:24:47 -0300

    arrumei o botão de menu

--- js.script.js
+++ js.script.js
@@ -1,5 +1,35 @@
 
-let btn = document.querySelector('.toggle');
+    function change (iconID){
+    if(document.getElementById(iconID).className=="fa fa-gamepad") {
+      document.getElementById(iconID).className = "fa fa-close";
+      
+    } else {
+      document.getElementById(iconID).className = "fa fa-gamepad";
+     
+      
+    } if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    }
+    else {
+        itens.style.display = 'block'
+    }
+    };
+  
+
+/*
+let btn = document.getElementById('joy');
+let icon = btn.document.getElementById('burguer');
+
+btn.onclick = function clickMenu() {
+if (itens.style.display == 'block') {
+    itens.style.display = 'none'
+}
+else {
+    itens.style.display = 'block'
+}
+};
+
+/*let btn = document.querySelector('.toggle');
 let icon = btn.querySelector('.fa-gamepad');
 
 btn.onclick = function() {
@@ -8,14 +38,4 @@ btn.onclick = function() {
     } else { 
         icon.classList.replace ('fa-gamepad');
     }
-} 
-
-
-function clickMenu() {
-if (itens.style.display == 'block') {
-    itens.style.display = 'none'
-}
-else {
-    itens.style.display = 'block'
-}
-}
+} */
